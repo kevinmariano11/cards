76a13b2af914e42dd902b52e4fb9ee553da6c047
Kavin mariano <00001091998747sp@al.educacao.sp.gov.br>
2024-11-26 19:46:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O q é o ciclo da agua?',
+    'é o movimento contínuo da água na natureza, que ocorre entre os oceanos, continentes e atmosfera'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual o estado solido da agua?',
+    'Gelo'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual a primeira lei de Newton?',
+    'um corpo em repouso ou em movimento retilíneo uniforme (MRU) permanecerá nesse estado a menos que uma força externa atue sobre ele'
+)
+
+criaCartao(
+    'Inglês',
+    'What is H2O?',
+    'H2O is Water'
+)
